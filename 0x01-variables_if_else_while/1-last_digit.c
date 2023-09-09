d9b89b8872b03afa390a8225690e46a383233952
Modupe <Teddydupsy@gmail.com>
2023-09-09 15:41:38 -0700

    If statement again

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -11,7 +11,7 @@ int main(void)
     int n;
     srand(time(0));
     n = rand() - RAND_MAX / 2;
-    printf("%d", n "is" n);
+    printf("%d ", n "is" n);
     if (n > 5)
     {
         printf("and is greater than 5");
