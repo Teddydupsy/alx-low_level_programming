f82b680a88682e804cb8323a3ace13d12e34329c
Modupe <Teddydupsy@gmail.com>
2023-09-10 15:27:06 -0700

    lowercase UPPERCASE

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - lower and UPPERCASE
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphaBET;
+
+	for (alphaBET = 'a'; alphaBET <= 'z'; alphaBET++)
+	{
+		putchar(alphaBET '\n');
+	}
+	for (alpaBET = 'A'; alphaBET <= 'Z' ; alphaBET++)
+	{
+		putchar(alphaBET '\n';)
+	}
+	return (0);
+}
