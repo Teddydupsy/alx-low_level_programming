9ada4d652e545422016fc5ff566de1daa078f625
Modupe <Teddydupsy@gmail.com>
2023-09-10 16:19:20 -0700

    Single digits of base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - Print all single digits of base from 0
+ * Return: Always 0
+ */
+int main(void)
+{
+	int Numbers;
+	
+	for (Numbers = '0'; Numbers < 10 ; Numbers++)
+	{
+		printf(Numbers);
+	}
+	Printf('\n');
+	return (0);
+}
