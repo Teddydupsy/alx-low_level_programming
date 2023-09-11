e8cbf8c928f483d88246cf61562479afb1254b98
Modupe <Teddydupsy@gmail.com>
2023-09-10 17:35:54 -0700

    Reverse alphabets

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - All in block
+ * Decription: Print all lowercase letters in reverse
+ * Use only 'putchar'
+ * Return: 0
+ */
+int main(void)
+{
+	char Rev_let;
+	
+	for (Rev_let = 'z'; Rev_let >= 'a'; Rev_let--)
+	{
+		putchar(Rev_let);
+	}
+	putchar('\n');
+	return (0);
+}
