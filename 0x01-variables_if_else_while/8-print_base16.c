25cc0cbde1f884bb7b8ffb855b3d8de641edaabe
Modupe <Teddydupsy@gmail.com>
2023-09-10 18:00:05 -0700

    Lowercase hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Print all the numbers of base 16 in lowercase
+ * Return: 0
+ */
+int main(void)
+{
+	/*Declare two variables of type 'int' and 'char'*/
+	int num;
+	char alpha;
+
+	for (num = 48; num <= 58; num++)
+	{
+		putchar(num);
+	}
+	for (alpha = 'a'; alpha < 'g'; alpha++)
+	{
+		putchar(alpha);
+	}
+	putchar('\n');
+	return (0);
+}
