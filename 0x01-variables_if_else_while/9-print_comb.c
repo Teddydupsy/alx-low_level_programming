2619274808a56f14e3786536896ac36f26635321
Modupe <Teddydupsy@gmail.com>
2023-09-10 18:44:13 -0700

    Single digit combo

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - All code in main block
+ * Description: All possible combination of single-digits
+ * Return: 0
+ */
+int main(void)
+{
+	int digit;
+
+	for (digit = 48; digit <= 57; digit ++)
+	{
+		putchar(digit);
+	if (x < 57)
+	{
+		putchar(44);
+		putchar(32);
+	}
+	}
+	putchar('\n');
+	return (0);
+}
