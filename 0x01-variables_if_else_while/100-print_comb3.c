d365be84392a8f349b699e9757da88237cb878d3
Modupe <Teddydupsy@gmail.com>
2023-09-11 17:56:13 -0700

    Number combination

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,12 +10,12 @@ int main(void)
 {
 	int num1, num2;
 
-	for (num1 = 48; num1 < 57; num1++)
+	for (num1 = 48; num1 <= 57; num1++)
 	{
-		for (num2 = 48; num2 < 57; num2++)
+		for (num2 = num1 + 1; num2 <= 57; num2++)
 		{
-			putchar(num1 + '0');
-			putchar(num2 + '0');
+			putchar(num1);
+			putchar(num2);
 
 			if (num1 < 8 || num2 < 9)
 			{
