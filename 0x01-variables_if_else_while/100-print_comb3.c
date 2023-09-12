52d95474dddfc488c941f84ceb4b040a0b766a73
Modupe <Teddydupsy@gmail.com>
2023-09-11 17:35:14 -0700

    Number combination

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - Print combinations of two different numbers
+ * Deecription: The combinations should be separated by comma followed by a space
+ * Numbers should be printed in ascending order using 'putchar'
+ * Do not use a variable of type 'char'
+ * Return: 0
+ */
+int main(void)
+{
+	int num1, num2;
+
+	for (num1 = 48; num1 < 57; num1++)
+	{
+		for (num2 = 48; num2 < 57; num2++)
+		{
+			putchar(num1);
+			putchar(num2);
+
+			if (num1 == 9 && num2 == 9)
+				continue;
+
+			putchar(44);
+			putchar(32);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
