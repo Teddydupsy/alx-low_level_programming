cb741d9b29aabfaf2a712f14c0edc380f6079e7a
Modupe <Teddydupsy@gmail.com>
2023-09-12 16:55:21 -0700

    Functions

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - To print _putchar
+ * Description: _putchar is used to print
+ * Return: 0
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
