53676f7b322c52857f63c38f19a125c10c3cfcf1
Modupe <Teddydupsy@gmail.com>
2023-09-11 19:20:47 -0700

    World eating software

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,9 +10,9 @@ int main(void)
 {
 	int num_1, num_2;
 
-	for (num_1 = 0; num_1 < 99; num_1++)
+	for (num_1 = 0; num_1 < 9; num_1++)
 	{
-		for (num_2 = num_1 + 1; num_2 <= 99; num_2++)
+		for (num_2 = num_1 + 1; num_2 <= 9; num_2++)
 		{
 			putchar((num_1 / 10) + '0');
 			putchar((num_1 % 10) + '0');
