55f81ff88b641bcce25c1e06bec91108543e952b
Modupe <Teddydupsy@gmail.com>
2023-09-12 18:37:40 -0700

    Absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _abs - Compute absolute value of an integer
+ * Declare a variable of type int
+ * Return: the absolute value of that integer
+ */
+int _abs(int)
+{
+	int var;
+
+	if (var >= 0)
+	{
+		return (var);
+	}
+	return (-var);
+}
