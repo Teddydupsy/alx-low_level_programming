2203c2fb410462adf1a8cd44063d4da191166752
Modupe <Teddydupsy@gmail.com>
2023-09-14 04:20:54 -0700

    Making function work

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * positive_or_negative - Testing the function
+ */
+void positive_or_negative(int i)
+{
+	if (i > 0)
+	{
+	printf("%dd is positive", i);
+	}
+	else if (i == 0)
+	{
+	printf("%d is zero", i);
+	}
+	else
+	{
+		printf("%d is negative\n", i);
+	}
+}
