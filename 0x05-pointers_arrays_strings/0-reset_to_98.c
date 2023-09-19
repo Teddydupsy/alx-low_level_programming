51f40df018abf172ed5a79eeb056384036984873
Modupe <Teddydupsy@gmail.com>
2023-09-19 06:47:36 -0700

    Testing my pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include <main.h>
+/**
+ * reset-to-98 - Reset pointer value to 98
+ * *n: is the pointer
+ * Return: Always 0.
+ */
+void reset_to_98(int *n)
+{
+	int a = 98
+	int *n;
+
+	n = &a
+}
