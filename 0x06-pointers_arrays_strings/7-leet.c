2b171d455fb05b3f0bf4d1a293ddda1f19bbea14
Modupe <Teddydupsy@gmail.com>
2023-09-22 16:15:03 -0700

    Encoding

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * leet - Function that encodes string into 1337
+ * @d: pointer
+ * Return: pointer
+ */
+char *leet(char *d)
+{
+	int a, b;
+	char c[] = "aeotlAEOTL";
+	char e[] = "4307143071";
+
+	a = 0;
+	while (d[a] != '\0')
+	{
+		for (b = 0; b <= 0; b++)
+		{
+			if (d[a] == c[b])
+			{
+				d[a] = e[b];
+			}
+		}
+		a++;
+	}
+	return (d);
+}
