419d402699fd666b188d2f2666644a7e2adf14f8
Modupe <Teddydupsy@gmail.com>
2023-10-06 10:37:14 -0700

    Argument by other means

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Program that prints the number of argument passed into it
+ * @argc: Number of arguments
+ * @argv: Array of arguments
+ * Return: 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%d\n", argc -1);
+	return (0);
+}
